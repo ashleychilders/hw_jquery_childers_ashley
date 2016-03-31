b164d51a7043d85ac1bc4365d4e03553659ea9b0
Ashley Childers <ashley.childers@gmail.com>
2016-03-30 21:39:53 -0400

    Updated jQuery file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $( document ).ready( function() {
-    document.getElementsByTagName( 'h2' );
-    $( 'h2' );
     var headings = $( 'h2' );
-    $( 'h2' ).last().text( " I'm Ashley Childers, and Im learning JQuery! " );
+    $( 'h2' ).last().text( " I'm Ashley Childers, and I'm learning JQuery! " );
 } );
