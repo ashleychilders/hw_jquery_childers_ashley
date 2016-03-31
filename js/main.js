40f103dab72ebdcae29dc52471b3913f10cdefd5
Ashley Childers <ashley.childers@gmail.com>
2016-03-30 22:39:07 -0400

    Add css objects

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready( function() {
+    var paragraphs = $( 'p' );
+    $( 'p' ).css( {
+        'color': 'DarkSlateBlue',
+        'background-color': 'gold',
+        'line-height': 2
+    } );
+} );
